43557cdb2ef81ec014c4115d23ef6f564decd011
GUSTAVO <gustavofont10@gmail.com>
2026-02-11 21:21:06 -0300

    Adicionando item 4 - Eventos

---
+++ 04-TemperatureMonitor/TemperatureMonitor/Program.cs
@@ -0,0 +1,43 @@
+﻿using System.Data;
+
+namespace TemperatureMonitor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            TemperatureSensor sensor = new TemperatureSensor();
+            sensor.TemperatureSense += Alert;
+
+            Console.WriteLine("Monitor de Temperatura Iniciado.");
+            while (true)
+            {
+                Console.WriteLine("Informe as temperaturas: ");
+                if (double.TryParse(Console.ReadLine(), out double inputTemp))
+                {
+                    sensor.Monitor(inputTemp);
+                }
+
+            }
+
+            static void Alert(double temp)
+            {
+                Console.WriteLine($"Perigo! {temp} graus!");
+            }
+        }
+    }
+
+    public class TemperatureSensor
+    {
+        public event Action<double> TemperatureSense;
+
+        public void Monitor(double temperature)
+        {
+            if (temperature > 99)
+            {
+                TemperatureSense?.Invoke(temperature);
+            }
+        }
+    }
+}
