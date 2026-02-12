765c3666557b89e3bf866046967ea6234028e4ac
GUSTAVO <gustavofont10@gmail.com>
2026-02-11 22:12:12 -0300

    Item 6 - MulticastDelegate

---
+++ 06-MulticastDelegate/MulticastDelegate/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace MulticastDelegate
+{
+
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+           Logger logs = new Logger();
+
+            Action<string> Multicast = logs.LogToConsole;
+            Multicast += logs.LogToFile; 
+            Multicast += logs.LogToDatabase;
+
+            Multicast("Registrados");
+        }
+    }
+
+    public class Logger
+    {
+        public void LogToConsole(string msg)
+        {
+            Console.WriteLine("Log no console:" + msg);
+        }
+
+        public void LogToFile(string msg)
+        {
+            Console.WriteLine("Log no File: " + msg);
+        }
+
+        public void LogToDatabase(string msg)
+        {
+            Console.WriteLine("Log na Db: " + msg);
+        }
+    }
+}
