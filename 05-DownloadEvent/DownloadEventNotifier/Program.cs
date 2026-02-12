1c177692847e6ea29f4b21816dc2e34f617e7627
GUSTAVO <gustavofont10@gmail.com>
2026-02-11 21:50:59 -0300

    Adicionando Item 5 - DownloadEventNotifier Threads

---
+++ 05-DownloadEvent/DownloadEventNotifier/Program.cs
@@ -0,0 +1,40 @@
+﻿using System.Threading;
+
+namespace DownloadEventNotifier
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            DownloadManager manager = new DownloadManager();
+
+            manager.DownloadComplete += Alert;
+
+            manager.IniciarDownload();
+        }
+
+        public static void Alert(string arquivo)
+        {
+            Console.WriteLine("O seu download foi concluido: " + arquivo);
+        }
+    }
+
+    public class DownloadManager
+    {
+        public event Action<string> DownloadComplete;
+
+        public void IniciarDownload()
+        {
+            Console.WriteLine("Download iniciado...");
+            Console.WriteLine("Finaliando em 10s...");
+
+            Thread.Sleep(10000);
+
+            DownloadComplete?.Invoke("TP1.zip");
+        }
+
+
+    }
+}
