5cc0a5bad85fc01d1e08b53f130ac1d702bd4333
GUSTAVO <gustavofont10@gmail.com>
2026-02-20 20:04:42 -0300

    Adicionando ex 11.

---
+++ 11-DelegatesEncadeados/ManipulacaoDeStringsComDelegatesEncadeados/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace ManipulacaoDeStringsComDelegatesEncadeados
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Func<string, string, string> concatenate = (nome, sobrenome) =>
+            {
+                string resultado = nome + " " + sobrenome;
+                Console.WriteLine(resultado);
+                return resultado;
+            };
+
+            concatenate += (nome, sobrenome) =>
+            {
+                string concatUpper = $"{nome} {sobrenome}".ToUpper();
+                Console.WriteLine(concatUpper);
+                return concatUpper;
+            };
+
+            concatenate += (nome, sobrenome) =>
+            {
+                string concatNoSpace = $"{nome} {sobrenome}".Replace(" ", "");
+                Console.WriteLine(concatNoSpace);
+                return concatNoSpace;
+            };
+
+            
+
+            string final = concatenate("Gustavo", "Fontoura");
+        }
+    }
+}
